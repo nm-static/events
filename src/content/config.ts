d1b66318c0fad2b2f67a157fc73dd6120c644b7b
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-01-27 01:03:00 +0200

    added content collections - meta tags

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,47 @@
+
 import { defineCollection, z } from "astro:content";
-
-const postsCollection = defineCollection({
-    schema: z.object({
-      title: z.string(),
-      pubDate: z.date(),
-      description: z.string(),
-      author: z.string(),
-
-      image: z.object({
+const directory = defineCollection({
+  schema: z.object({
+    pubDate: z.date(),
+    visitUrl: z.string(),
+    websiteName: z.string(),
+    description: z.string(),
+    category: z.array(z.string()),
+    typography: z.array(z.string()),
+    thumbnail: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    images: z.array(
+      z.object({
         url: z.string(),
-        alt: z.string()
-      }),
+        alt: z.string(),
+      })
+    ),
+  }),
+});
+const infopages = defineCollection({
+  schema: z.object({
+    page: z.string(),
+  }),
+});
+const postsCollection = defineCollection({
 
-      tags: z.array(z.string())
-    })
- });
+  schema: z.object({
 
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  }),
+});
 export const collections = {
   posts: postsCollection,
+  infopages: infopages,
+  directory: directory,
 };
